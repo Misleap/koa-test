d02b3a63b8bc18b5c53f14e5142aa55e24501c89
mis <18970065896@163.com>
2016-12-21 16:07:12 +0800

    version 3.0: add url(POST) support,add bodyparser

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const Koa = require('koa');
 
 //注意返回的是函数
 const router = require('koa-router')();
-// const bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
-
+app.use(bodyParser());
 app.use(async(ctx, next) => {
 	await next();	//next 是koa传入的将要处理的下一个异步函数
 	// ctx.response.type = 'text/html';
@@ -17,14 +17,29 @@ app.use(async(ctx, next) => {
 	console.log(`${ctx.response.status}`);
 });
 
-router.get('/hello/:name', async(ctx, next) => {
-	var name = ctx.params.name;
-	ctx.response.body = `<h1>HELLO, ${name}</h1>`;
+router.get('/', async(ctx, next) => {
+	ctx.response.body = `<h1>login panel</h1>
+		<form action="/login" method="POST">
+			username:<input type="text" name="username"></input><br>
+			password:<input type="password" name="password"></input><br>
+			<input type="submit" value="Submit"></input>
+		</form>
+	`;
 });
 
-router.get('', async(ctx, next) => {
-	ctx.response.body = `<h1>hello, man</h1>`;
+router.post('/login', async(ctx, next) => {
+	var name = ctx.request.body.username || '',
+		password = ctx.request.body.password || '';
+	console.log(`login with name:${name}, password:${password}`);
+	if (name === 'koa' && password === '123') {
+		ctx.response.body = `<h2>welcome, ${name}</h2>`;
+	} else {
+		ctx.response.body = `<h2>login failed!</h2>
+		<p><a href="/">Try Again</a></p>`;
+	}
 });
+
+
 
 app.use(router.routes());	//add router middleware
 
