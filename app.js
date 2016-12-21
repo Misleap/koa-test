a40fb30e92121530a68bbeeb6c1a77f6551972b7
mis <18970065896@163.com>
2016-12-21 15:06:05 +0800

    version 2.0:handle for url(GET) router

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 // var koa = require('koa');
 // var app = koa();
 
-// app.use('/test', function);
 const Koa = require('koa');
+
+//注意返回的是函数
+const router = require('koa-router')();
+// const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
 app.use(async(ctx, next) => {
-	await next();
-	ctx.response.type = 'text/html';
-	ctx.response.body = '<h2>Hello, KOA2</h2>'
+	await next();	//next 是koa传入的将要处理的下一个异步函数
+	// ctx.response.type = 'text/html';
+	// ctx.response.body = '<h2>Hello, KOA2</h2>'
 	console.log(`${ctx.request.method} ${ctx.request.url}`);
 	console.log(`${ctx.response.status}`);
-	// console.log('${ctx.request.method} ${ctx.request.url}');
-
 });
 
-//next 是koa传入的将要处理的下一个异步函数
+router.get('/hello/:name', async(ctx, next) => {
+	var name = ctx.params.name;
+	ctx.response.body = `<h1>HELLO, ${name}</h1>`;
+});
+
+router.get('', async(ctx, next) => {
+	ctx.response.body = `<h1>hello, man</h1>`;
+});
+
+app.use(router.routes());	//add router middleware
 
 app.listen(3000);
 console.log('app started at port 3000...');
